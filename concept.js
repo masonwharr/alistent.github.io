e9c3c6bb5c4f07c4cb33458fdb98f5066a49f44f
AListEnt <36723785+AListEnt@users.noreply.github.com>
2018-03-02 12:07:54 -0500

    Update concept.js

--- concept.js
+++ concept.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
 	var removeThese = [];
+	var overdueTasks = ["lllll", "lll"];
+var overdue = overdueTasks.length;
+	$('.tasksDetails').each(function() {
+var theTab = $(this);
+	var theTabSlide = theTab[0].nextElementSibling;
+	var theTabRule = $(theTabSlide).find('.slider');
+	var theTabColor = ($(theTabRule).css("background-color"));
+		console.log(theTabColor);
+	
+		$(this).html('<div class="tasksDetailsBox" style="padding-left: 8px; border-left: 4px solid ' + theTabColor + '">' + '<p class="tasksName">' + 'Pick up groceries' + '</p>' + '<p class="tasksDate">' + 'Date' + '</p>' + '</div>');
+
+});
+	/*$('.slider').each(function() {
+		var sliderColor = $(this).css("background-color");
+		//var sliderData = $(this).dataset.bg;
+		console.log(sliderColor);
+
+	});*/
+	$('.overdueBox').html('<p class="overdueText">' + 'You have ' + overdue + ' OVERDUE TASKS. </p>');
+	if (overdue == 0){
+		$('.overdueBox').remove();
+	}
 	$('span[data-date]').each(function() {
 		// Eventually change to if data-date seconds since 1970 is less than todays amount since 1970, its overdue.
 		if ($(this).attr('data-date') == "date1") {
